61ad34b315f6353765b2751a9551570da7546f15
rajankouzina <rajang@kftpl.com>
2021-12-05 12:14:49 +0530

    Backend hello World

---
+++ index.js
@@ -0,0 +1,9 @@
+//to run : node filename.js
+const express = require('express')
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
